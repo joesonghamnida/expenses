9112c1d75516922f4fa47b308ad61c4893f1e82a
Joe Morris <joe.morris.dev@gmail.com>
2017-11-18 10:54:38 +0100

    refactored design

--- src/IncomeDDL.java
+++ src/IncomeDDL.java
@@ -3,14 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DDL {
+public class IncomeDDL {
 
     public static Connection connection() throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
         return conn;
     }
 
-    public static void createDatabase(Connection conn)throws SQLException{
+    public static void createIncomeDatabase(Connection conn)throws SQLException{
+        Statement statement = conn.createStatement();
+        statement.execute("CREATE TABLE IF NOT EXISTS income(id IDENTITY income VARCHAR amount INTEGER payDate VARCHAR payment_source VARCHAR)");
+    }
+
+    public static void createExpensesDatabase(Connection conn)throws SQLException{
         Statement statement = conn.createStatement();
         statement.execute("CREATE TABLE IF NOT EXISTS expenses(id IDENTITY expense VARCHAR amount INTEGER payment_source VARCHAR expense_type INTEGER)");
     }
---
+++ src/IncomeDML.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class IncomeDML {
+
+    //TODO: deal with timestamps and selecting a date
+    public void insertIntoExpenses(Connection conn, String expense, int amount, String dueDate, String paymentSource, int expenseType)throws SQLException{
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO expenses VALUES (NULL ,?,?,?,?,?)");
+        statement.setString(1, expense);
+        statement.setInt(2,amount);
+        statement.setString(3, dueDate);
+        statement.setString(4, paymentSource);
+        statement.setInt(5, expenseType);
+    }
+}
