bd30ebd1f11be3b4d065a83c848f1c2c8a92f023
Joe Morris <joe.morris.dev@gmail.com>
2017-11-26 04:59:09 +0100

    updated

--- src/ExpenseDML.java
+++ src/ExpenseDML.java
@@ -1,2 +1,31 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ExpenseDML {
+
+    //TODO: deal with timestamps and selecting a date
+    public void insertIntoExpenses(Connection conn, String expense, int amount, String dueDate, String paymentSource, int expenseType)throws SQLException {
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO expenses VALUES (NULL ,?,?,?,?,?)");
+        statement.setString(1, expense);
+        statement.setInt(2,amount);
+        statement.setString(3, dueDate);
+        statement.setString(4, paymentSource);
+        statement.setInt(5, expenseType);
+    }
+
+    public void updateExpenses(Connection conn)throws SQLException{
+        PreparedStatement statement = conn.prepareStatement("");
+
+        //TODO: find something more elegant than if / else
+
+    }
+
+    public void getExpenses()throws SQLException{
+
+    }
+
+    public void removeExpense()throws SQLException{
+
+    }
 }
--- src/IncomeDML.java
+++ src/IncomeDML.java
@@ -5,12 +5,25 @@ import java.sql.SQLException;
 public class IncomeDML {
 
     //TODO: deal with timestamps and selecting a date
-    public void insertIntoExpenses(Connection conn, String expense, int amount, String dueDate, String paymentSource, int expenseType)throws SQLException{
-        PreparedStatement statement = conn.prepareStatement("INSERT INTO expenses VALUES (NULL ,?,?,?,?,?)");
-        statement.setString(1, expense);
-        statement.setInt(2,amount);
-        statement.setString(3, dueDate);
-        statement.setString(4, paymentSource);
-        statement.setInt(5, expenseType);
+    public void insertIntoIncome(Connection conn, String incomeSource, int amount, String depositDate)throws SQLException{
+        PreparedStatement statement = conn.prepareStatement("INSERT INTO income VALUES (NULL ,?,?,?)");
+        statement.setString(1, incomeSource);
+        statement.setInt(2, amount);
+        statement.setString(3, depositDate);
+    }
+
+    public void updateIncome(Connection conn)throws SQLException{
+        PreparedStatement statement = conn.prepareStatement("");
+
+        //TODO: find something more elegrant than if / else
+
+    }
+
+    public void getIncome()throws SQLException{
+
+    }
+
+    public void removeIncome()throws SQLException{
+
     }
 }
